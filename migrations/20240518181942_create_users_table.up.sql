e4f500ae180ef54a07574ed00d3d0b4ecb423172
Felipe Fernandes Versiane <felipeversiane09@gmail.com>
2024-05-18 16:03:10 -0300

    chore: adding a auto migration service

---
+++ migrations/20240518181942_create_users_table.up.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
+        document VARCHAR(255) NOT NULL,
+        balance NUMERIC(10, 2) NOT NULL DEFAULT 0,
+        is_merchant BOOLEAN NOT NULL DEFAULT FALSE,
+        created_at TIMESTAMP DEFAULT now(),
+        updated_at TIMESTAMP  DEFAULT now()
+);

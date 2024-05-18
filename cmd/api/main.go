67f58c417ff67e0a2400af1c6385ad3f04d97b8f
Felipe Fernandes Versiane <felipeversiane09@gmail.com>
2024-05-18 00:41:24 -0300

    chore: adding a db connection configuration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"context"
+	"os"
+
 	"github.com/felipeversiane/picpay-golang.git/bootstrap"
+	"github.com/felipeversiane/picpay-golang.git/config/db"
 	"github.com/felipeversiane/picpay-golang.git/config/logger"
 	"go.uber.org/zap"
+)
+
+var (
+	POSTGRES_URL = "POSTGRES_URL"
 )
 
 func init() {
@@ -22,5 +30,18 @@ func init() {
 }
 
 func main() {
+	var err error
+	ctx := context.Background()
+	connectionString := os.Getenv(POSTGRES_URL)
+
+	conn, err := db.NewConnection(ctx, connectionString)
+	if err != nil {
+		logger.Fatal("Database error: ", err,
+			zap.String("journey", "Database Connection"))
+	}
+	defer conn.Close()
+
+	logger.Info("Database connection completed",
+		zap.String("journey", "Database Connection"))
 
 }
